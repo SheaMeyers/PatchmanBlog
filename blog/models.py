53ffa168eaaf7fefbb5ea5195af6733ab2e77740
SheaMeyers <shea.meyers@gmail.com>
2016-07-01 17:27:21 -0600

    Created Post model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('auth.User')
+    created_date = models.DateField(default=timezone.now)
+    content = models.TextField()
+
+    is_published = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        ordering=['-created_date']
+
+    def publish(self):
+        self.save()
