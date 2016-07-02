afe7de9c8f91f8e881d6059ae4fd16398b7dbdf8
SheaMeyers <shea.meyers@gmail.com>
2016-07-02 15:41:48 -0600

    Have post data being populated on main page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'post_list.html', {})
+    posts = Post.objects.filter(is_published=True).order_by('created_date')
+    return render(request, 'post_list.html', {'posts': posts})
