8257767071ff3dfc4fd5c7a7ed981cf4cab06b39
SheaMeyers <shea.meyers@gmail.com>
2016-07-03 19:53:05 -0600

    Have a post detail page for a user to go to to see the entire post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
-def post_list(request):
+def home(request):
     posts = Post.objects.filter(is_published=True).order_by('created_date')
-    return render(request, 'post_list.html', {'posts': posts})
+    return render(request, 'home.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post })
