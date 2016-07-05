b62c9f524856cde87054a09dc99455aaf40d7dd6
SheaMeyers <shea.meyers@gmail.com>
2016-07-04 20:41:01 -0600

    Have form so a user can add a new post.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
+from .models import Post
+from .forms import PostForm
 
 def home(request):
     posts = Post.objects.filter(is_published=True).order_by('created_date')
@@ -9,4 +11,18 @@ def home(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'post_detail.html', {'post': post })
+    return render(request, 'post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_new.html', {'form': form})
