02a2a404023903a2bb1bfa80ccc019dc0bab30cc
SheaMeyers <shea.meyers@gmail.com>
2016-07-10 14:43:13 -0600

    Wrote unit tests to cover model functions.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from models import Post, Reply
 
-# Create your tests here.
+
+class PostVisibilityTests(TestCase):
+    def setUp(self):
+        self.fake_user = User(username='FakeUser', password='FakePassword')
+        self.fake_user.save()
+        self.fake_post = Post(title='Fake Post', author=self.fake_user, content='This is a fake post used for testing')
+
+    def test_post_publish_method_sets_is_published_to_True(self):
+        self.fake_post.publish()
+        self.assertTrue(self.fake_post.is_published)
+
+    def test_post_unpublish_method_sets_is_publised_to_False(self):
+        self.fake_post.unpublish()
+        self.assertFalse(self.fake_post.is_published)
+
+    def test_post_delete_method_sets_is_deleted_to_True(self):
+        self.fake_post.delete()
+        self.assertTrue(self.fake_post.is_deleted)
+
+
+class ReplyVisibilityTests(TestCase):
+    def setUp(self):
+        self.fake_user = User(username='FakeUser', password='FakePassword')
+        self.fake_user.save()
+        self.fake_post = Post(title='Fake Post', author=self.fake_user, content='This is a fake post used for testing')
+        self.fake_post.save()
+        self.fake_reply = Reply(post=self.fake_post, name='FakeName', email='fake@email.com',
+                                content='This is fake reply to be used for testing')
+
+    def test_reply_approve_method_sets_is_approved_to_True(self):
+        self.fake_reply.approve()
+        self.assertTrue(self.fake_reply.approved_reply)
+
+    def test_reply_delete_method_set_is_deleted_to_True(self):
+        self.fake_reply.delete()
+        self.assertTrue(self.fake_reply.is_deleted)
